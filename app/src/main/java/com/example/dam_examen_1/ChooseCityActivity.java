f56504c6e5859995e64599f3c43de85e2fc5328c
jhon <deynercatacora@upeu.edu.pe>
2019-09-06 18:44:55 -0500

    add card_view_bg file drawable

--- app/src/main/java/com/example/dam_examen_1/ChooseCityActivity.java
+++ app/src/main/java/com/example/dam_examen_1/ChooseCityActivity.java
@@ -2,6 +2,8 @@ package com.example.dam_examen_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ChooseCityActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ChooseCityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_city);
+        /*Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.countries_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);*/
     }
+
 }
