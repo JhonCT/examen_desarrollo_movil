2a44167cfce85250153d0dbf84a3dc925f796ff3
jhon <deynercatacora@upeu.edu.pe>
2019-09-07 18:46:16 -0500

    wip page 2

--- app/src/main/java/com/example/dam_examen_1/ChooseCityActivity.java
+++ app/src/main/java/com/example/dam_examen_1/ChooseCityActivity.java
@@ -1,22 +1,30 @@
 package com.example.dam_examen_1;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ChooseCityActivity extends AppCompatActivity {
+    private ListView listView;
+
+    private String countries [] = {"Pakistán", "Palaos", "Panamá", "Paraguay", "Perú", "Polonia",
+            "Portugal", "Reino Unido", "República Centroafricana", "República Checa", "República del Congo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_city);
-        /*Spinner spinner = findViewById(R.id.spinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.countries_arrays, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);*/
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_countries, countries);
+        listView.setBackgroundColor(Color.WHITE);
+        listView.setAdapter(adapter);
+
     }
 
 }
