4e59f96611c24f02ecd5ddc8901b6acfa4ce66f5
lega122 <alonsoarambulo@upeu.edu.pe>
2019-09-09 00:50:20 -0500

    loquis

---
+++ app/src/main/java/com/example/dam_examen_1/login_bola_mayor.java
@@ -0,0 +1,13 @@
+package com.example.dam_examen_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_bola_mayor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_bola_mayor);
+    }
+}
